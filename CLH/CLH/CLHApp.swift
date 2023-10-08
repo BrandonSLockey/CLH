8be7195614b710f6a4bae80c41313eb20c72ec38
BrandonSLockey <72495991+BrandonSLockey@users.noreply.github.com>
2023-10-08 08:55:59 +0100

    Xcode Start

---
+++ CLH/CLH/CLHApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLHApp.swift
+//  CLH
+//
+//  Created by Brandon S Lockey on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CLHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
